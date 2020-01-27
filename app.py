be5d9f624373bb70ccbb8fcc2874e04110bbe90a
Trent Hendrickson <trentdev2020@gmail.com>
2020-01-27 16:47:38 -0700

    added a post route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
@@ -11,7 +11,7 @@ app = Flask(__name__)
 #     return "Hey Flask"
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.splite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
@@ -20,9 +20,10 @@ class Guide(db.Model):
     title = db.Column(db.String(100), unique=False)
     content = db.Column(db.String(144), unique=False)
 
-    def __int__(self, title, content):
+    def __init__(self, title, content):
         self.title = title
         self.content = content
+
 
 class GuideSchema(ma.Schema):
     class Meta:
@@ -32,8 +33,21 @@ class GuideSchema(ma.Schema):
 guide_schema = GuideSchema()
 guides_schema = GuideSchema(many=True)
 
+# Endpoint to create a new guide
+@app.route('/guide', methods=["POST"])
+def add_guide():
+    title = request.json['title']
+    content = request.json['content']
+
+    new_guide = Guide(title, content)
+
+    db.session.add(new_guide)
+    db.session.commit()
+
+    guide = Guide.query.get(new_guide.id)
+
+    return guide_schema.jsonify(guide)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
